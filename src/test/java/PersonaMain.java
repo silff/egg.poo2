6de31986340a5a66e06005aadc556a81a4b253fe
silff <128050929+silff@users.noreply.github.com>
2023-04-24 16:05:23 -0300

    actualizacion

--- src/test/java/PersonaMain.java
+++ src/test/java/PersonaMain.java
@@ -6,7 +6,8 @@ public class PersonaMain {
     public static void main(String[] args) {
         PersonaService persona1 = new PersonaService();
         Persona pers1 = persona1.crearPersona();
-        System.out.println("Nombre: " + pers1.getNombre() + "\nEdad: " + pers1.getEdad() + "\nDNI: " +pers1.getDni());
+        System.out.println("Nombre: " + pers1.getNombre() + "\nEdad: " 
+                + pers1.getEdad() + "\nDNI: " +pers1.getDni());
     }
     
 }
