aa0e25c7592e1dac9060712b6e4a1f312b9b045a
silff <128050929+silff@users.noreply.github.com>
2023-04-25 11:11:44 -0300

    Pedro

--- src/main/java/Entities/Cafetera.java
+++ src/main/java/Entities/Cafetera.java
@@ -2,18 +2,7 @@
 Programa Nespresso. Desarrolle una clase Cafetera en el paquete Entidades con los 
 atributos capacidadMáxima (la cantidad máxima de café que puede contener la cafetera) 
 y cantidadActual (la cantidad actual de café que hay en la cafetera). Agregar
-constructor vacío y con parámetros así como setters y getters. Crear clase
-CafeteraServicio en el paquete Servicios con los siguiente:
-Método llenarCafetera(): hace que la cantidad actual sea igual a la capacidad máxima. 
-Método servirTaza(int): se pide el tamaño de una taza vacía, el método recibe el 
-tamaño de la taza y simula la acción de servir la taza con la capacidad indicada. 
-Si la cantidad actual de café “no alcanza” para llenar la taza, se sirve lo que quede.
-El método le informará al usuario si se llenó o no la taza, y de no haberse llenado 
-en cuanto quedó la taza.
-Método vaciarCafetera(): pone la cantidad de café actual en cero. 
-Método agregarCafe(int): se le pide al usuario una cantidad de café, el método 
-lo recibe y se añade a la cafetera la cantidad de café indicada.
-
+constructor vacío y con parámetros así como setters y getters. 
  */
 package Entities;
 
@@ -23,6 +12,31 @@ public class Cafetera {
     int cantidadMaxima;
     int cantidadActual;
     
+    //constructors
+    public Cafetera() {
+    }
+
+    public Cafetera(int cantidadMaxima, int cantidadActual) {
+        this.cantidadMaxima = cantidadMaxima;
+        this.cantidadActual = cantidadActual;
+    }
+    
+    //getters y setters
+    public int getCantidadMaxima() {
+        return cantidadMaxima;
+    }
+
+    public void setCantidadMaxima(int cantidadMaxima) {
+        this.cantidadMaxima = cantidadMaxima;
+    }
+
+    public int getCantidadActual() {
+        return cantidadActual;
+    }
+
+    public void setCantidadActual(int cantidadActual) {
+        this.cantidadActual = cantidadActual;
+    }
     
     
 }
--- src/main/java/Servicies/CafeteraServicios.java
+++ src/main/java/Servicies/CafeteraServicios.java
@@ -1,13 +1,33 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+Crear clase
+CafeteraServicio en el paquete Servicios con los siguiente:
+-Método llenarCafetera(): hace que la cantidad actual sea igual a la capacidad máxima. 
+-Método servirTaza(int): se pide el tamaño de una taza vacía, el método recibe el 
+tamaño de la taza y simula la acción de servir la taza con la capacidad indicada. 
+Si la cantidad actual de café “no alcanza” para llenar la taza, se sirve lo que quede.
+El método le informará al usuario si se llenó o no la taza, y de no haberse llenado 
+en cuanto quedó la taza.
+-Método vaciarCafetera(): pone la cantidad de café actual en cero. 
+-Método agregarCafe(int): se le pide al usuario una cantidad de café, el método 
+lo recibe y se añade a la cafetera la cantidad de café indicada.
  */
 package Servicies;
 
-/**
- *
- * @author Silvana
- */
+import Entities.Cafetera;
+import java.util.Scanner;
+
 public class CafeteraServicios {
     
+    static Scanner sc = new Scanner(System.in);
+    static Cafetera cafetera = new Cafetera();
+    
+    public void llenarCafetera() {
+        cafetera.setCantidadMaxima(100);
+        cafetera.setCantidadActual(cafetera.getCantidadMaxima());
+    }
+    
+     public void servirTaza() {
+            
+         
+        }
 }
